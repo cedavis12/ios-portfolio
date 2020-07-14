12822b4e0843dca2e43509c26523e4b8479fcdf7
Courtney Davis <cedavis12@gmail.com>
2020-07-13 22:35:04 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//Welcome-Section Scroll Effect
+//scroll to top on page refresh (PART1)
+window.onbeforeunload = function () {
+window.scrollTo(0, 0);
+};
+//for arrow click part (PART2)
+$(document).ready( function () {
+$('#down').on('click',function () {
+$("html").scrollTop(0);
+$('html, body').animate({
+	scrollTop: $('#about').offset().top
+}, 500);
+});
+});
+
+//Navbar Scroll Effect
+$(function(){
+	$(document).scroll(function(){
+		var $nav = $("#main-nav");
+		var $welcome = $("#welcome-section")
+		$nav.toggleClass("scrolled" , $(this).scrollTop() > $welcome.height());
+	});
+});
+
+//Bootstrap ScrollSpy
+$('body').scrollspy({ target: '#main-nav' })
